fbb8303da999b07958b811a751dbc0cde19215a5
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-17 22:29:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  listed = ""
+  
+if array.length > 2 
+  shrt_ar = []
+  for item in 0..array.length-2
+    shrt_ar.push(array[item])
+  end
+  
+  listed = "#{shrt_ar.join(", ")}, and #{array[array.length-1]}"
+  
+  elsif array.length == 2
+  listed = array.join(" and ")
+else 
+  listed = array.join()
 end
+end
+
+puts oxford_comma(["meow", "mix", "dynamite"])
